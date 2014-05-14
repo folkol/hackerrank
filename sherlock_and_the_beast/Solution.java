eb813352b36ffafef53214a420626878a47fa46c
Mattias Johansson <mattias.johansson@atex.com>
2014-05-14 10:38:52 +0200

    Added solution for Sherlock and The Beast

---
+++ sherlock_and_the_beast/Solution.java
@@ -0,0 +1,39 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+
+public class Solution {
+
+    static Scanner input = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        @SuppressWarnings("unused")
+            int numTests = input.nextInt();
+
+        while (input.hasNextInt()) {
+            System.out.println(findDecentNumber(input.nextInt()));
+        }
+    }
+
+    static String findDecentNumber(int numDigits) {
+        for (int i = numDigits, j = 0; i >= 0; i--, j++) {
+            if ((i % 3 == 0) && (j % 5 == 0)) {
+                return decentNumber(i, j);
+            }
+        }
+        return "-1";
+    }
+
+    private static String decentNumber(int i, int j) {
+        StringBuilder sb = new StringBuilder(i + j);
+        sb.append(repdigit(i, '5'));
+        sb.append(repdigit(j, '3'));
+        return sb.toString();
+    }
+
+    private static char[] repdigit(int i, char digit) {
+        char[] number = new char[i];
+        Arrays.fill(number, digit);
+        return number;
+    }
+}
